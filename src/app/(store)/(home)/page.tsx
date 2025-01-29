8f426aefa6633a1cf01818a3cff51ee627168c05
Gabriel Castro <gabrielcssg@gmail.com>
2025-01-29 00:11:58 -0300

    feat: product page, metadata

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { api } from "@/data/api";
 import { Product } from "@/data/types/products";
+import { Metadata } from "next";
 
 async function getFeaturedProducts(): Promise<Product[]> {
   const response = await api("/products/featured", {
@@ -14,13 +15,17 @@ async function getFeaturedProducts(): Promise<Product[]> {
   return products;
 }
 
+export const metadata: Metadata = {
+  title: "Home",
+};
+
 export default async function Home() {
   const [highlightedProduct, ...otherProducts] = await getFeaturedProducts();
 
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={`/products/${highlightedProduct.slug}`}
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
@@ -46,7 +51,7 @@ export default async function Home() {
       {otherProducts.map((product) => (
         <Link
           key={product.id}
-          href={`/products/${product.slug}`}
+          href={`/product/${product.slug}`}
           className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
         >
           <Image
